21a8acc53b9d9e7f214ed2ae1d84f20ee33ddca9
rrassya19-bit <rrassys19@gmail.com>
2026-04-03 15:22:02 +0700

    Tambah looping input elemen array beserta tampilan index

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -20,4 +20,14 @@ void input()
             cout << "\nMaksimal panjang array adalah 20";
         }
     }
+
+    cout << "\n--------------------------" << endl;
+    cout << "\nInputkan Isi elemnt array" << endl;
+    cout << "\n--------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
 }
